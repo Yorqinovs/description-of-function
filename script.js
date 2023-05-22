87e5d92a91ea7ff12afaba128db7d3bc8154edd7
Yorqinovs <you@example.com>
2023-05-22 05:53:32 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+"use stcit";
+
+// console.log("Open modal");
+// function sayHelloUzbekistan() {
+//   console.log("Hello Uzbekistan");
+// }
+// sayHelloUzbekistan();
+
+// //<<Function decloration>>
+// function calc(a, b) {
+//   console.log(a + b);
+// }
+// calc(10, 5);
+// calc(20, 8);
+
+// let age = 30;
+// function sayMyName(name, surename) {
+//   //   let age = 22;
+//   age = 20;
+//   console.log(`My name is ${name} ${surename}`);
+//   console.log(age);
+// }
+// sayMyName("Sunnatillo", "Yorqinov");
+// console.log(age);
+
+// function calc(a, b) {
+//   return a + b;
+// }
+// console.log(calc(20, 8));
+
+// //<<Expression function>>
+// const logger = function () {
+//   console.log("Hey What's up ?");
+// };
+// logger();
+
+// console.log(sumOfAandB);
+// function calc(a, b) {
+//   return a + b;
+// }
+
+// //<<Arrow function>>
+// const calc = (a, b) => a + b;
+// console.log(calc(10, 8));
